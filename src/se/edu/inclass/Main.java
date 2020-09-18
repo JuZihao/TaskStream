875eaa81c79779ca2a7b7e9731b7eb885fb39e79
JuZihao <57383789+JuZihao@users.noreply.github.com>
2020-09-18 17:52:28 +0800

    Added filterByString

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -8,6 +8,8 @@ import se.edu.inclass.task.TaskNameComparator;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
 
     private TaskNameComparator taskNameComparator;
@@ -21,6 +23,8 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+        printDeadlineUsingStreams(tasksData);
+        System.out.println(filterByString(tasksData));
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -45,5 +49,19 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlineUsingStreams(ArrayList<Task> tasksData) {
+        tasksData.stream()
+                .filter((s)-> s instanceof Deadline)
+                .sorted((a, b) -> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
+        .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterByString(ArrayList<Task> tasksData,String filterString) {
+        ArrayList<Task> filteredTaskList = tasksData.stream()
+                .filter((s) -> s.getDescription().contains(filterString))
+                .collect(toList());
+        return  filteredTaskList;
     }
 }
